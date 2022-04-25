07c9a712b3f840d640d6690c969a4ef1b7bc1e19
aoshaaa <kalachev.artemm@gmail.com>
2022-04-25 23:34:26 +0300

    task5 v1

---
+++ src/main/java/com/kalachev/task5/LruCache.java
@@ -0,0 +1,32 @@
+package com.kalachev.task5;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LruCache {
+	Map<String, String> cache;
+	int capacity;
+
+	public LruCache(int capacity) {
+		super();
+		this.cache = new LinkedHashMap<>();
+		this.capacity = capacity;
+	}
+
+	public void add(String input,String output) {
+		if (cache.size() == capacity) {
+			String firstKey = cache.keySet().iterator().next();
+           cache.remove(firstKey);
+		}
+		cache.put(input, output);
+	}
+	
+	public String displayCacheContentKeys(LruCache lruCache) {
+		StringBuilder sb = new StringBuilder();
+		for(String s: lruCache.cache.keySet()) {
+			sb.append(s + ",");
+		}
+		sb.deleteCharAt(sb.length()-1);
+		return sb.toString();
+	}
+}
---
+++ src/main/java/com/kalachev/task5/UniqueCharacters.java
@@ -0,0 +1,69 @@
+package com.kalachev.task5;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class UniqueCharacters {
+	static final String NEWLINE = System.lineSeparator();
+	private StringBuilder createdOutoutSourcesString;
+	
+	public UniqueCharacters() {
+		this.createdOutoutSourcesString = new StringBuilder();
+	}
+	
+	public String printUniqs(String input, LruCache lru) throws IllegalArgumentException {
+		checkForNull(input);
+		if (lru.cache.containsKey(input)) {
+			createdOutoutSourcesString.append(getCacheSource(input));
+			return lru.cache.get(input);
+		} else {
+			String printedOutputString = doThePrint(input);
+			lru.add(input,printedOutputString);
+			createdOutoutSourcesString.append(getCalculationSource(input));
+			return printedOutputString;
+		}
+	}
+	
+	public String outputSourseHistory() {
+		return createdOutoutSourcesString.toString();
+	}
+	
+	private static LinkedHashMap<Character, Integer> createOutputMap(String input) {
+		LinkedHashMap<Character, Integer> uniqCharactersMap = new LinkedHashMap<>();
+		for (Character c : input.toCharArray()) {
+			if (!uniqCharactersMap.containsKey(c)) {
+				uniqCharactersMap.put(c, 1);
+			} else {
+				uniqCharactersMap.put(c, uniqCharactersMap.get(c) + 1);
+			}
+		}
+		return uniqCharactersMap;
+	}
+	
+	private static String doThePrint(String input) {
+		LinkedHashMap<Character, Integer> uniqCharactersMap = createOutputMap(input);
+		StringBuilder sb = new StringBuilder();
+		sb.append(input);
+		if(!input.isEmpty()) {
+			sb.append(NEWLINE);
+		}
+		for (Map.Entry<Character, Integer> entry : uniqCharactersMap.entrySet()) {
+			sb.append("\"" + entry.getKey() + "\"" + " - " + entry.getValue() + NEWLINE);
+		}
+		return sb.toString();
+	}
+
+	private static String getCacheSource(String input) {
+		return input +NEWLINE + "this string was outputed from cache" + NEWLINE;
+	}
+	private static String getCalculationSource(String input) {
+		return input +NEWLINE + "this string was outputed from calculations" + NEWLINE;
+	}
+	
+	private static void checkForNull(String input) {
+		if(input == null) {
+			throw new IllegalArgumentException();
+		}
+	}
+}
+
